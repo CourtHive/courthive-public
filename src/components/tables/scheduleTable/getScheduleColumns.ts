afc48fccf45513a933efc4018c03db3db8dd264b
CourtHive <courthive@gmail.com>
2024-03-26 17:57:52 -0400

    feat: :construction: add schedule table components

---
+++ src/components/tables/scheduleTable/getScheduleColumns.ts
@@ -0,0 +1,26 @@
+import { generateEmptyColumns } from './generateEmptyColumns';
+import { getControlColumn } from './getControlColumn';
+import { scheduleCell } from './scheduleCell';
+
+import { CENTER, MINIMUM_SCHEDULE_COLUMNS } from 'src/common/constants/baseConstants';
+
+export function getScheduleColumns({ courtsData, courtPrefix }) {
+  const columnsCalc = MINIMUM_SCHEDULE_COLUMNS - courtsData?.length || 0;
+  const emptyColumnsCount = columnsCalc <= 0 ? 1 : columnsCalc;
+
+  const emptyColumns = generateEmptyColumns({ courtsData, count: emptyColumnsCount });
+  const controlColumn = getControlColumn();
+
+  const generateColumn = (courtInfo, index) => ({
+    field: `${courtPrefix}${index}`,
+    title: courtInfo.courtName,
+    headerHozAlign: CENTER,
+    formatter: scheduleCell,
+    headerSort: false,
+    resizable: false,
+    hozAlign: CENTER,
+    minWidth: 150
+  });
+
+  return [controlColumn].concat(courtsData?.map(generateColumn), emptyColumns);
+}
