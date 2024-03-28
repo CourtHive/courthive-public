5b2dbaed67f7f5a077cfc0ef001d90590b9c62ea
CourtHive <courthive@gmail.com>
2024-03-28 07:45:30 -0400

    feat: :sparkles: date selector for schedule tab; cleanup

--- src/components/tables/scheduleTable/createScheduleTable.ts
+++ src/components/tables/scheduleTable/createScheduleTable.ts
@@ -1,51 +1,70 @@
+import { dropDownButton } from 'src/components/buttons/dropDownButton';
+import { removeAllChildNodes } from 'src/services/dom/transformers';
 import { TabulatorFull as Tabulator } from 'tabulator-tables';
+import { scheduleGovernor } from 'tods-competition-factory';
 import { destroyTable } from 'src/components/destroyTable';
 import { getScheduleColumns } from './getScheduleColumns';
-import { tools } from 'tods-competition-factory';
+import dayjs from 'dayjs';
 
 export function createScheduleTable(params) {
-  const { scheduledDate, data } = params ?? {};
-  let existingCourtIds = [];
+  const { dateMatchUps = [], completedMatchUps = [], groupInfo } = params?.data ?? {};
+  const matchUps = dateMatchUps.concat(...completedMatchUps);
+  const scheduleDates = matchUps.reduce((dates, matchUp) => {
+    const scheduledDate = matchUp?.schedule?.scheduledDate;
+    if (scheduledDate && !dates.includes(scheduledDate)) dates.push(scheduledDate);
+    return dates;
+  }, []);
+
+  const scheduledDate = scheduleDates[0];
   let table: any = undefined;
-  let ready;
 
-  const getTableData = (params) => {
-    !!params;
-    const { dateMatchUps = [], completedMatchUps = [], courtsData, courtPrefix = 'C|', rows, groupInfo } = data;
-    const matchUps = dateMatchUps.concat(...completedMatchUps);
+  const getTableData = ({ scheduledDate }) => {
+    const courtsData = params?.data?.courtsData.map((court) => {
+      const { matchUps, ...details } = court;
+      return { ...details, matchUps: matchUps.filter((matchUp) => matchUp.schedule?.scheduledDate === scheduledDate) };
+    });
 
+    const courtPrefix = 'C|';
+    const rows = scheduleGovernor.courtGridRows({ courtsData, courtPrefix, minRowsCount: 10 }).rows;
     const columns: any = getScheduleColumns({ courtsData, courtPrefix });
 
     rows?.forEach((row, i) => {
       row.rowId = `rowId-${i + 1}`;
       row.rowNumber = i + 1;
     });
-    return { rows, columns, matchUps, courtsCount: courtsData?.length ?? 0, courtsData, groupInfo };
+    return { rows, columns, courtsCount: courtsData?.length ?? 0, courtsData, groupInfo };
   };
 
   const replaceTableData = ({ scheduledDate }) => {
-    const refresh = () => {
-      const { rows, matchUps, columns, courtsData } = getTableData({ scheduledDate });
-      const courtIds = courtsData?.map((court) => court.courtId);
+    const { rows } = getTableData({ scheduledDate });
 
-      const equivalentCourts = tools.intersection(existingCourtIds, courtIds).length === courtIds?.length;
-
-      if (!equivalentCourts) {
-        table.setColumns(columns);
-        existingCourtIds = courtIds;
-      }
-      table?.replaceData(rows);
-      table.matchUps = matchUps;
-    };
-
-    setTimeout(refresh, ready ? 0 : 1000);
+    table?.replaceData(rows);
+    table.matchUps = matchUps;
   };
+
+  const formatDate = (dateString) => dayjs(dateString).format('dddd MMM D');
+  const dateOptions = scheduleDates.map((dateString) => ({
+    onClick: () => replaceTableData({ scheduledDate: dateString }),
+    label: formatDate(dateString),
+    value: dateString,
+    close: true
+  }));
+  const dateSelector = {
+    label: formatDate(scheduledDate),
+    options: dateOptions,
+    id: 'dateSelector',
+    modifyLabel: true,
+    selection: true
+  };
+  const scheduleHeader = document.getElementById('scheduleHeader');
+  removeAllChildNodes(scheduleHeader);
+  const elem = dropDownButton({ button: dateSelector });
+  scheduleHeader.appendChild(elem);
 
   destroyTable({ anchorId: 'tournamentSchedule' });
   const element = document.getElementById('tournamentSchedule');
 
   const { rows = [], columns = [], courtsCount } = getTableData({ scheduledDate });
-  existingCourtIds = columns.map((col) => col?.courtId).filter(Boolean);
 
   table = new Tabulator(element, {
     height: window.innerHeight * 0.84,
@@ -56,5 +75,5 @@ export function createScheduleTable(params) {
     columns
   });
 
-  return { table, replaceTableData, courtsCount };
+  return { table, courtsCount };
 }
--- src/components/tables/scheduleTable/generateEmptyColumns.ts
+++ src/components/tables/scheduleTable/generateEmptyColumns.ts
@@ -4,17 +4,9 @@ import { scheduleCell } from './scheduleCell';
 // constants
 import { CENTER } from 'src/common/constants/baseConstants';
 
-export function generateEmptyColumns({ courtsData, count }) {
-  const emptyColumnHeader = (index) => {
-    if (index) return undefined;
-
-    return courtsData?.length || 0
-      ? `<p style='font-weight: normal; color: lightblue'>Add venue</p>`
-      : `<button class='button is-danger'>Add venue</button>`;
-  };
+export function generateEmptyColumns({ count }) {
   return count > 0
-    ? tools.generateRange(0, count).map((index) => ({
-        title: emptyColumnHeader(index),
+    ? tools.generateRange(0, count).map(() => ({
         headerHozAlign: CENTER,
         formatter: scheduleCell,
         headerSort: false,
